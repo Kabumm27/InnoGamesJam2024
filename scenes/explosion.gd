7ece9d4b3458a2ae916d00ec0e33dadf8d5b5ef1
Kim Mannstedt <k.mannstedt@gmail.com>
2024-11-09 18:22:23 +0100

    added damage for explosions

--- scenes/explosion.gd
+++ scenes/explosion.gd
@@ -7,3 +7,7 @@ func _ready() -> void:
 
 func animation_finished() -> void:
 	await explosion.animation_finished
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if(body.has_method('reduce_health')):
+		body.reduce_health(10)
