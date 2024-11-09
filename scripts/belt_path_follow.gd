4ff14eaf9c0a2e57420449560621580a63f4d35c
Low <julian.gill@web.de>
2024-11-09 17:15:46 +0100

    Pathing in progress

---
+++ scripts/belt_path_follow.gd
@@ -0,0 +1,17 @@
+extends PathFollow2D
+
+@onready var belt_path_follow: PathFollow2D = $"."
+
+@export var duration :float = 10.0
+
+func _ready():
+  belt_path_follow.progress_ratio = 0
+
+  var tween :Tween = create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+  tween.tween_property(belt_path_follow, 'progress_ratio', 1, duration)
+
+
+#var _speed :float = 200.0
+#
+#func _physics_process(delta):
+	#belt_path_2_follow.set_progress(belt_path_2_follow.get_progress() + _speed * delta)
---
+++ scripts/belt_pickup_zone.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+@onready var belt_path: Path2D = $"../BeltPath"
+
+const BELT_PATH_FOLLOW = preload("res://scenes/BeltPathFollow.tscn")
+
+func _on_body_entered(body: Node2D):
+		print("Entered")
+		var bomb := body as CharacterBody2D
+		if bomb:
+			if bomb.name == "Bomb":
+				var beltPathFollowInstance = BELT_PATH_FOLLOW.instantiate()
+				beltPathFollowInstance.add_child(bomb)
+				belt_path.add_child(beltPathFollowInstance) 
+				print("Bomb")
+				print(belt_path)
+			else:
+				print("No Bomb")
