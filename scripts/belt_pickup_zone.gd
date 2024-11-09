51463f57b021cfa1470d98c95a860957b3db72bf
Low <julian.gill@web.de>
2024-11-09 21:27:08 +0100

    Fixed Belts for other items

--- scripts/belt_pickup_zone.gd
+++ scripts/belt_pickup_zone.gd
@@ -6,24 +6,24 @@ const BELT_PATH_FOLLOW = preload("res://scenes/BeltPathFollow.tscn")
 
 func _on_body_entered(body: Node2D):
 		print("Entered")
-		var bomb := body as CharacterBody2D
-		if bomb:
-			if bomb.name == "Bomb":
-				if bomb.has_method("toggle_collision"):
-					bomb.toggle_collision(false)
-					
-				var beltPathFollowInstance = BELT_PATH_FOLLOW.instantiate()
+		var item := body as CharacterBody2D
+		if item:
+			#if item.name == "item":
+			if item.has_method("toggle_collision"):
+				item.toggle_collision(false)
 				
-				bomb.position = Vector2.ZERO
-				belt_path.add_child(beltPathFollowInstance)
-				
-				bomb.get_parent().remove_child.call_deferred(bomb)
-				await get_tree().create_timer(0.05).timeout
-				beltPathFollowInstance.add_child(bomb)
+			var beltPathFollowInstance = BELT_PATH_FOLLOW.instantiate()
+			
+			item.position = Vector2.ZERO
+			belt_path.add_child(beltPathFollowInstance)
+			
+			item.get_parent().remove_child.call_deferred(item)
+			await get_tree().create_timer(0.05).timeout
+			beltPathFollowInstance.add_child(item)
 
-				#beltPathFollowInstance.global_position = Vector2.ZERO
-				
-				print("Bomb")
-				#print(belt_path)
-			else:
-				print("No Bomb")
+			#beltPathFollowInstance.global_position = Vector2.ZERO
+			
+			print("item")
+			#print(belt_path)
+		else:
+			print("No item")
