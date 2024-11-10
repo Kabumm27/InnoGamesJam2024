067de5926106785c51320fa4ae0840ea1b09ca9a
Kim Mannstedt <k.mannstedt@gmail.com>
2024-11-10 14:45:58 +0100

    replace object when leaving belt when already occupied

--- scripts/belt_end_zone.gd
+++ scripts/belt_end_zone.gd
@@ -1,5 +1,25 @@
 extends Area2D
 
+@onready var map: Map = $%Map
+
+var bomb_placed = false
+
 func _on_body_entered(body: Node2D) -> void:
+	
+	if bomb_placed:
+		var tilemap_pos = map.global_to_tilemap(global_position)
+		
+		var e = tilemap_pos
+		e.x -= 1
+		
+		body.global_position = map.tilemap_to_global(e)
+	else:
+		bomb_placed = true
+	
 	if body.has_method('light_up'):
 		body.light_up()
+
+
+func _on_body_exited(body: Node2D) -> void:
+	bomb_placed = false
+	print('body left', body)
--- scripts/fire.gd
+++ scripts/fire.gd
@@ -16,7 +16,6 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	if(body.has_method('reduce_health')):
 		body.reduce_health(1)
 
-
 func _on_burning_time_timeout() -> void:
 	fire.stop()
 	fire.hide()
