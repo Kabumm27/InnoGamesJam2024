70607da76c346ba555cff04a3da765909b4ea651
Low <julian.gill@web.de>
2024-11-10 16:34:11 +0100

    Added main menu and credits music. added credits back button.

--- scripts/button_exitCredits.gd
+++ scripts/button_exitCredits.gd
@@ -1,23 +1,21 @@
 extends Button
 
-const EXIT_BUTTON = preload("res://assets/sprites/exit_button.png")
-const EXIT_BUTTON_HOVER = preload("res://assets/sprites/exit_button_hover.png")
+const BACK_BUTTON = preload("res://assets/sprites/back_button.png")
+const BACK_BUTTON_HOVER = preload("res://assets/sprites/back_button_hover.png")
 
 var _active = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	icon = EXIT_BUTTON
+	icon = BACK_BUTTON
 
 func _on_button_mouse_entered() -> void:
 	_active = true
-	icon = EXIT_BUTTON_HOVER
-
+	icon = BACK_BUTTON_HOVER
 
 func _on_button_mouse_exited() -> void:
 	_active = false
-	icon = EXIT_BUTTON
-	
+	icon = BACK_BUTTON
 	
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
