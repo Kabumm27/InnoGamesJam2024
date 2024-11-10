7feaabcba7d67683c2c7405be9bfaa6e338837ac
kslowikowski <kristof@slowikowski.net>
2024-11-10 16:56:40 +0100

    fixed buttons

--- scripts/game_menu.gd
+++ scripts/game_menu.gd
@@ -12,7 +12,8 @@ func show_winning_screen(team: Enums.Team):
 
 
 func _on_play_again_pressed():
-	get_tree().change_scene_to_file("res://scenes/main.tscn")
+	get_tree().paused = false
+	get_tree().reload_current_scene()
 
 
 func _on_back_to_menu_pressed():
